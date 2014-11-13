d57ee7b683547205ccbb21ed92e5fc2f3577428f
Ryan Kim <nainirajk@gmail.com>
2014-11-13 20:54:48 +0900

    Adds update action the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -35,8 +35,7 @@ RSpec.describe Api::V1::UsersController, :type => :controller do
     context "when is not created" do
       before(:each) do
         # notice I'm not including the email
-        @invalid_user_attributes = { password: '12345678',
-                                     password_confirmation: '12345678'}
+        @invalid_user_attributes = { password: '12345678', password_confirmation: '12345678'}
         post :create, { user: @invalid_user_attributes }, format: :json
       end
 
@@ -51,10 +50,45 @@ RSpec.describe Api::V1::UsersController, :type => :controller do
       end
 
       it { should respond_with 422 }
+    end # when is not created
+  end # post
+
+  describe "PUT/PATCH #update" do
+    context "when is successfully updated" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id,
+                         user: { email: "newmail@example.com" } }, format: :json
+      end
+
+      it "renders the json representation for the updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql "newmail@example.com"
+      end
+
+      it { should respond_with 200 }
+    end # when is successfully updated
+
+    context "when is not created" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id,
+                         user: { email: "bademail.com" } }, format: :json
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+
+      it { should respond_with 422 }
 
     end # when is not created
-
-
-  end # post
+  end # PUT/PATCH #update
 
 end # end
