8815e931089d3925ea9f7df6c47746b31bfcf048
Ryan Kim <nainirajk@gmail.com>
2014-11-19 20:19:07 +0900

    Finishes modeling the product model along with user associations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -22,4 +22,6 @@ describe Product, :type => :model do
   it { is_expected.to validate_presence_of :price }
   it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
   it { is_expected.to validate_presence_of :user_id }
+
+  it { is_expected.to belong_to :user }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,8 @@ RSpec.describe User, :type => :model do
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_confirmation_of(:password) }
   it { is_expected.to allow_value('example@domain.com').for(:email) }
+
+  it { is_expected.to have_many(:products)}
   # describe "when email is not present" do
   #   before {@user.email = " "}
 
@@ -34,6 +36,21 @@ RSpec.describe User, :type => :model do
       existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
       @user.generate_authentication_token!
       expect(@user.auth_token).not_to eql existing_user.auth_token
+    end
+  end
+
+  describe "#products association" do
+    before do
+      @user.save
+      3.times { FactoryGirl.create :product, user: @user}
+    end
+
+    it "destroys the associated products on self destruct" do
+      products = @user.products
+      @user.destroy
+      products.each do |product|
+        expect(Product.find(product)).to raise_error ActiveRecord::RecordNotFound
+      end
     end
   end
 end
