ae7a2950cba6b9001744c7587b5ee709c99a8380
Ryan Kim <nainirajk@gmail.com>
2014-11-27 17:18:41 +0900

    Adds search class method to filter products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -57,12 +57,26 @@ describe Product, :type => :model do
     end
   end # .below_or_equal_to_price
 
+  describe ".above_or_equal_to_price" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100
+      @product2 = FactoryGirl.create :product, price: 50
+      @product3 = FactoryGirl.create :product, price: 150
+      @product4 = FactoryGirl.create :product, price: 99
+    end
+
+    it "returns the products which are above or equal to the price" do
+      expect(Product.above_or_equal_to_price(100).sort).to match_array([@product1, @product3])
+    end
+  end
+
   describe ".recent" do
     before(:each) do
       @product1 = FactoryGirl.create :product, price: 100
       @product2 = FactoryGirl.create :product, price: 50
       @product3 = FactoryGirl.create :product, price: 150
       @product4 = FactoryGirl.create :product, price: 99
+
       @product2.touch
       @product3.touch
     end
@@ -72,5 +86,41 @@ describe Product, :type => :model do
       expect(Product.recent).to match_array([@product3, @product2, @product4, @product1])
     end
 
-  end
+  end # .recent
+
+  describe ".search" do
+    before(:each) do
+      @product1 = FactoryGirl.create :product, price: 100, title: "Plasma tv"
+      @product2 = FactoryGirl.create :product, price: 50, title: "Videogame console"
+      @product3 = FactoryGirl.create :product, price: 150, title: "MP3"
+      @product4 = FactoryGirl.create :product, price: 99, title: "Laptop"
+    end
+
+    context "when title 'videogame' and '100' a min price are set" do
+      it "returns an empty array" do
+        search_hash = { keyword: "videogame", min_price: 100 }
+        expect(Product.search(search_hash)).to be_empty
+      end
+    end
+
+    context "when title 'tv', '150' as max price, and '50' as min price are set" do
+      it "returns the product1" do
+        search_hash = { keyword: "tv", min_price: 50, max_price: 150 }
+        expect(Product.search(search_hash)).to match_array([@product1])
+      end
+    end
+
+    context "when an empty hash is sent" do
+      it "returns all the products" do
+        expect(Product.search({})).to match_array([@product1, @product2, @product3, @product4])
+      end
+    end
+
+    context "when product_ids is present" do
+      it "returns the product from the ids" do
+        search_hash = { product_ids: [@product1.id, @product2.id]}
+        expect(Product.search(search_hash)).to match_array([@product1, @product2])
+      end
+    end
+  end # .search
 end
